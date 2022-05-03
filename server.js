03521222c3360b8331961b9f30363ca1b65aeafe
faelz123 <rafael2@outlook.com>
2022-05-03 18:58:15 -0300

    connect to database

--- server.js
+++ server.js
@@ -1,6 +1,22 @@
 import express from 'express';
 import cors from 'cors';
+import knex from 'knex';
 import bcrypt from 'bcryptjs';
+
+const postgres = knex({
+    client: 'pg',
+    connection: {
+      host : '127.0.0.1',
+      port : 5432,
+      user : 'postgres',
+      password : '9595',
+      database : 'smartbrain'
+    }
+  });
+
+postgres.select('*').from('users').then(data => {
+    console.log(data);
+});
 
 const app = express();
 app.use(express.urlencoded({extended: false}));
